8e85e3db7a71bc3957e46b1d9fe081765e63f407
Rafaela <rafaela.vm@gmail.com>
2017-01-13 16:19:27 -0200

    alteracoes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,77 +1,80 @@
 angular.module('starter.controllers', [])
 
-.controller('ProdutoController', function ($scope, $ionicModal) {
+  .controller('ProdutoController', function ($scope, $ionicModal) {
 
-    $scope.produtos = [
-        {
-            titulo: 'Livro de Ionic',
-            valor: 50.99,
-            publicar: true
-        },
-        {
-            titulo: 'Livro de Nodejs',
-            valor: 75.00,
-            publicar: true
-        },
-        {
-            titulo: 'Livro de CSS3',
-            valor: 100,
-            publicar: true
-        },
-        {
-            titulo: 'Aprenda html em 5 dias',
-            valor: 30,
-            publicar: true
-        },
-        {
-            titulo: 'Conhecendo o javascript na prática',
-            valor: 69.99,
-            publicar: false
-        },
+    $scope.produtos = [{
+        titulo: 'Livro de Ionic',
+        valor: 50.99,
+        publicar: true
+      },
+      {
+        titulo: 'Livro de Nodejs',
+        valor: 75.00,
+        publicar: true
+      },
+      {
+        titulo: 'Livro de CSS3',
+        valor: 100,
+        publicar: true
+      },
+      {
+        titulo: 'Aprenda html em 5 dias',
+        valor: 30,
+        publicar: true
+      },
+      {
+        titulo: 'Conhecendo o javascript na prática',
+        valor: 69.99,
+        publicar: false
+      },
 
-  ];
+    ];
 
-   $ionicModal.fromTemplateUrl('templates/adicionar.html',{
-  	scope:$scope
-  }).then(function(modal){
-  	$scope.modal = modal;
+    $ionicModal.fromTemplateUrl('templates/adicionar.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.abreAdicionar = function () {
+      $scope.editando = false;
+      $scope.produto.titulo = "";
+      $scope.produto.valor = "";
+      $scope.produto.publicar = false;
+      $scope.modal.show();
+    }
+
+    $scope.addProduto = function (produto) {
+      $scope.produtos.push({
+        titulo: produto.titulo,
+        valor: produto.valor,
+        publicar: produto.publicar
+      });
+
+      produto.titulo = "";
+      produto.valor = "";
+      produto.publicar = false;
+      $scope.modal.hide();
+    }
+
+    $scope.produto = {};
+    $scope.editando = false;
+    var auxProdutoEditar;
+
+    $scope.editarProduto = function (produtoEditar) {
+      $scope.editando = true;
+      $scope.produto.titulo = produtoEditar.titulo;
+      $scope.produto.valor = produtoEditar.valor;
+      $scope.produto.publicar = produtoEditar.publicar;
+      auxProdutoEditar = produtoEditar;
+      $scope.modal.show();
+    };
+
+    $scope.salvarProduto = function (produto) {
+      auxProdutoEditar.titulo = produto.titulo;
+      auxProdutoEditar.valor = produto.valor;
+      auxProdutoEditar.publicar = produto.publicar;
+      $scope.modal.hide();
+    };
+
   });
-
-  $scope.abreAdicionar = function(){
-  	$scope.editando = false;
-  	$scope.produto.titulo = "";
-  	$scope.produto.valor = "";
-  	$scope.produto.publicar = false;
-  	$scope.modal.show();
-  }
-
-  $scope.addProduto = function(produto){
-  	$scope.produtos.push({titulo: produto.titulo, valor: produto.valor, publicar:produto.publicar});
-  	produto.titulo = "";
-  	produto.valor = "";
-  	produto.publicar = false;
-  	$scope.modal.hide();
-  }
-
-  $scope.produto = {};
-  $scope.editando = false;
-  var auxProdutoEditar;
-
-  $scope.editarProduto = function(produtoEditar){
-  	$scope.editando = true;
-  	$scope.produto.titulo = produtoEditar.titulo;
-  	$scope.produto.valor = produtoEditar.valor;
-  	$scope.produto.publicar = produtoEditar.publicar;
-  	auxProdutoEditar = produtoEditar;
-  	$scope.modal.show();
-  };
-
-  $scope.salvarProduto = function(produto){
-  	auxProdutoEditar.titulo = produto.titulo;
-  	auxProdutoEditar.valor = produto.valor;
-  	auxProdutoEditar.publicar = produto.publicar;
-  	$scope.modal.hide();
-  };
-
-});
-
